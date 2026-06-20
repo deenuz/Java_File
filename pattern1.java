69b2757caa7e99e6d2bf93597abc9afc31505f30
Deendayal Rajak <125788029+deenuz@users.noreply.github.com>
2026-06-20 08:40:04 +0530

    Refactor star pattern printing into Pattern class
    Refactor pattern printing logic into a new class and method.

--- pattern1.java
+++ pattern1.java
@@ -1,20 +1,33 @@
 //First pattern question for star printing in squre shape
+/* * * * * 
+ * * * * * 
+ * * * * * 
+ * * * * * 
+ * * * * */
 import java.util.Scanner;
-public class pattern1 {
-    public static void main(String []args )
-    {
-        try (Scanner scan = new Scanner(System.in)) {
-            System.out.println("Enter any number");
 
-            int n = scan.nextInt();
-    for (int j=0;j<n;j++) 
-     {
-             for(int i=0; i<n;i++)
-            {
+public class Pattern {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter any number");
+        int n = scan.nextInt();
+
+        Pattern p = new Pattern();
+        p.printPattern(n);
+
+        scan.close();
+    }
+
+
+    public void printPattern(int n){
+         
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n; j++){
                 System.out.print("* ");
             }
-            System.out.println("*");
-     }
+
+            System.out.println("");
         }
-   }
+    }    
 }
