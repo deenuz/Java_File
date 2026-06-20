331cf0498f4aff176ff626eb37f5066c2018f5fd
Deendayal Rajak <125788029+deenuz@users.noreply.github.com>
2026-06-20 08:49:25 +0530

    Update and rename pattern2.java to  hollow_square.java

---
+++ hollow_square.java
@@ -0,0 +1,40 @@
+// * * * * * 
+// *       * 
+// *       * 
+// *       * 
+// * * * * * 
+// hollow square
+
+import java.util.Scanner;
+
+public class Pattern {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter any number");
+        int n = scan.nextInt();
+
+        Pattern p = new Pattern();
+        p.printPattern(n);
+
+        scan.close();
+    }
+
+
+    public void printPattern(int n){
+         
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                if(i==0||i==(n-1)||j==0||j==(n-1)){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+
+            System.out.println("");
+        }
+    }
+    
+}
+
